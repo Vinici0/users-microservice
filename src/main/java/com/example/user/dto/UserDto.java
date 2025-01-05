5b28016cda8d2444f9204fca6a91b3d12fff46ac
vinicio <vinicio.borja10@gmail.com>
2025-01-05 00:49:28 -0500

    Add DTOs, request/response classes, and repositories for cart and order management

--- src/main/java/com/example/user/dto/UserDto.java
+++ src/main/java/com/example/user/dto/UserDto.java
@@ -1,10 +1,17 @@
-package com.example.user.dto;
+package com.dailycodework.dreamshops.dto;
 
+import com.dailycodework.dreamshops.model.Cart;
+import com.dailycodework.dreamshops.model.Order;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class UserDto {
     private Long id;
-    private String nombre;
+    private String firstName;
+    private String lastName;
     private String email;
+    private List<OrderDto> orders;
+    private CartDto cart;
 }
